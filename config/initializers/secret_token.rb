0a7210cfd9c99807bbce87abd9ed8ec78603deac
johnvehr <johnvehr@yahoo.com>
2014-08-13 11:57:10 -0600

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaOAuth2::Application.config.secret_token = '8a6ba875ec862e1b1ee1cf76beca538fc9577a368ca97853c4715f333014fe60a462027563a1a699a4cc7fd22a35047e9e320afed53a55aff282ecf1206cd636'
